e7c5f24b21d818925a0f778968865dc293b3501f
lauralin04 <116968466+lauralin04@users.noreply.github.com>
2023-11-09 18:37:37 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Skills from "./components/Skills";
 
 export default function App() {
   return (
-    <main className="text-gray-200 bg-gray-900 body-font">
+    <main className="text-gray-300 bg-gray-800 body-font">
       <Navbar />
       <About />
       <Projects />
