aa5e5fa454e763dc34c666fce5f5f66081472b56
lauralin04 <116968466+lauralin04@users.noreply.github.com>
2024-07-31 13:47:51 -0400

    Updated some project descriptions

--- src/data.js
+++ src/data.js
@@ -8,7 +8,7 @@ export const projects = [
     link: "https://docs.google.com/document/d/1b8M_J-tMApZmvTunMwTcVlnVO5pEIh_f8IE6obFjS30/edit?usp=sharing",
   },
   {
-    title: "Speedy Fingies - Arcade Game (Completed)",
+    title: "Speedy Fingies - Arcade Game",
     subtitle: "Arduino ",
     description:
       "A game where users tap a sensor in time with dots displayed on an LED matrix. Build using an Arduino.",
@@ -19,17 +19,28 @@ export const projects = [
     title: "FPGA Rhythm Game (completed)",
     subtitle: "Verilog",
     description:
-      "A rhythm game built on the DE1-SOC board. The project includes VGA display, access to on baord RAM, and input from a ps2 keyboard. ",
+      "A rhythm game built on the DE1-SOC board. The project includes VGA display, access to on baord RAM, and input from a ps2 keyboard. I worked on audio encoding/decoding and integration of the components.",
     image: "./project_3.jpeg",
     link: "",
   },
   {
-    title: "Speedy Fingies 2.0 (completed)",
+    title: "Speedy Fingies 2.0",
     subtitle: "Arduino and Processing",
     description:
       "Rhythm game using Arduino, LED Matrix driver, and Processing to interface to computer speakers. ",
     image: "./project_4.jpeg",
     link: "https://devpost.com/software/speedy-fingies-2-0",
+  },
+  {
+    title: "3D Ping-Pong on FPGA Board",
+    subtitle: "Arduino and Processing",
+    description:
+      "Ping pong game writen for the Nios-II processor on DE!-SOC. I wrote the graphics engine in C, which\
+      used a 3D coordinate system, and basic raytracing algorithms to project the 3D objects onto\
+      a 2D plane. I also implemented background music using interupts, to ensure that music could \
+      play while graphics was being processed.",
+    image: "./project_3.jpeg",
+    link: "https://github.com/LancelotShih/bingpong",
   },
 ];
 
@@ -38,5 +49,6 @@ export const skills = [
   "C++ Object Oriented Programming",
   "Arduino/microcontroller programming",
   "Verilog",
-  "Hardware design"
+  "Robotics",
+  "Machine learning optimization"
 ];
